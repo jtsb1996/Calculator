2bf05e881c1c3948838a857e8138fc07e88c312c
jay <jaytan1996@gmail.com>
2021-03-10 16:59:09 +0800

    Added functions to calculator

--- calculator.java
+++ calculator.java
@@ -1,6 +1,31 @@
-class calculator{
-    public static void main(String[]args)
+class calculator {
+    public static void main(String[] args) {
+        System.out.println("This is a calculator test");
+    }
+
+    // function that sums two digits
+    public int add(int x, int y) {
+        int results = x + y;
+        return results;
+    }
+
+    // function that minus two digits
+    public int subtract(int x, int y) {
+        int results = x - y;
+        return results;
+    }
+
+    // function that multiplies two digits
+    public int multiply(int x, int y)
     {
-        
+        int results = x * y;
+        return results;
+    }
+
+    // function that divides two digits
+    public int divide(int x, int y)
+    {
+        int results = x / y;
+        return results;
     }
 }
